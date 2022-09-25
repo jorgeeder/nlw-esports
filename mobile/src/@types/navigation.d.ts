88e50a71d600a1ead146276752eec7019216a750
jorgeeder <jorgeeder.js@gmail.com>
2022-09-25 14:11:21 -0300

    feat(mobile): day 4 - created selected game page header

--- mobile/src/@types/navigation.d.ts
+++ mobile/src/@types/navigation.d.ts
@@ -1,12 +1,14 @@
+export interface GameParams {
+  id: string;
+  title: string;
+  bannerurl: string;
+}
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      game: {
-        id: string;
-        title: string;
-        bannerurl: string;
-      }
+      game: GameParams; 
     }
   }
 }
